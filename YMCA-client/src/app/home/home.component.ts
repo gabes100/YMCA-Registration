df6f26ef9ec4b9d12326eadf028754876ee338ce
gabe <gabesv100@gmail.com>
2020-12-07 00:00:08 -0600

    major bug fixs and minor refactor

--- YMCA-client/src/app/home/home.component.ts
+++ YMCA-client/src/app/home/home.component.ts
@@ -5,7 +5,7 @@ import { Router } from '@angular/router';
 import { User } from '../user';
 import { Program } from '../program';
 import { FormControl, FormGroup} from '@angular/forms';
-
+import Utils from '../utils'
 
 @Component({
   selector: 'app-home',
@@ -29,22 +29,25 @@ export class HomeComponent implements OnInit {
     private data : DataService) { }
 
   ngOnInit(): void {
+    // get user and navigate back if not found
     this.user = JSON.parse(localStorage.getItem('user-login'));
-  
+    
     if (!this.user){
       this.router.navigate(['/']);
     }
 
+    this.isStaff = this.user.staff; 
+    this.isMember = this.user.member;
+    this.isModding = false;
+
+    // get user programs
     this.api.getUserPrograms(this.user['_id']).subscribe(programs =>{
       this.signedUp = programs;
     });
 
-
-    this.isStaff = this.user.staff; 
-    this.isMember = this.user.member;
-
     this.updateView();
 
+    // setup reactive forms
     this.programForm = new FormGroup({
       _id: new FormControl(''),
       programName: new FormControl(''),
@@ -70,28 +73,70 @@ export class HomeComponent implements OnInit {
       fee: new FormControl(''),
       capacity: new FormControl(''),
     });
-
-    this.isModding = false;
   }
-  
 
+  /* signUp
+  / Makes a call to the apiService and trys to sign up a user for program 
+  */
   signUp(): void{
-
-    this.api.signUp(this.user['_id'], {programid : this.programId}).subscribe(
-      
-    );
-
-     alert("You are now signed up");
-     location.reload();
-     //this.updateView();
+    this.api.signUp(this.user['_id'], {programid : this.programId}).subscribe(result =>{
+      if (result){
+        alert("You are now signed up");
+        this.updateView();
+      }
+      else{
+        alert("Sign up failed!");
+      }
+    });
   }
 
+  /* updateOrCreateProgram
+  /  Creates or updates a program
+  */
+  updateOrCreateProgram() : void {
+    let days = [];
+
+    if (this.programForm.controls['monday'].value) days.push("Mon");
+    if (this.programForm.controls['tuesday'].value) days.push("Tues");
+    if (this.programForm.controls['wednesday'].value) days.push("Wed");
+    if (this.programForm.controls['thursday'].value) days.push("Thurs");
+    if (this.programForm.controls['friday'].value) days.push("Fri");
+    if (this.programForm.controls['saturday'].value) days.push("Sat");
+    if (this.programForm.controls['sunday'].value) days.push("Sun");
+
+    let body = {
+      name : this.programForm.controls['programName'].value,
+      location : this.programForm.controls['location'].value,
+      description : this.programForm.controls['description'].value,
+      fee : this.programForm.controls['fee'].value,
+      capacity : this.programForm.controls['capacity'].value,
+      startTime : Utils.createTime(this.programForm.controls['timeStart'].value),
+      endTime: Utils.createTime(this.programForm.controls['timeEnd'].value),
+      startDate : Utils.createDate(this.programForm.controls['dateStart'].value),
+      endDate : Utils.createDate(this.programForm.controls['dateEnd'].value),
+      day : days,
+    };
+
+    if (this.isModding) {
+      this.api.modifyProgram(this.programId, body).subscribe();
+    } else{
+    this.api.createProgram(body).subscribe();
+  }
+   this.updateView();
+ }
+
+  /* updateView
+  /  Updates programs from the database
+  */
   updateView(): void{
     this.api.getPrograms().subscribe(programs =>{
       this.programs = programs;
     });
   }
 
+  /* isSignedUp
+  /  Checks what programs a user is signed up for
+  */
   isSignedUp(program: Program) : boolean {
     let ret = false;
     this.signedUp.forEach(elem => {
@@ -102,6 +147,9 @@ export class HomeComponent implements OnInit {
     return ret;
   }
 
+  /* openProgramModal
+  /  Passes the data to the modal for signing up a user, also calculates discount
+  */
   openProgamModal(data : Program): void {
     const discount = Math.ceil(<number>data.fee *  0.5);
     const fee = this.isMember ? discount: data.fee;
@@ -115,115 +163,67 @@ export class HomeComponent implements OnInit {
      this.programId = data['_id'];
   }
 
-  resetForm(formName: FormGroup): void {
-    formName.reset();
+  /* openRemoveModal
+  /  Passes the data to the modal for removing a program
+  */
+  openRemoveModal(data: Program): void {
+    this.programId = data['_id'];
   }
 
+  /* openCreateModal
+  /  Passes the data to the modal for creating a program
+  */
+  openCreateModal(): void {
+    this.isModding = false;
+  }
+
+  /* openModifyModal
+  /  Passes the data to the modal for modifing a program as well as patching values to the form
+  */
   openModifyModal(data : Program): void {
     this.programId = data['_id'];
     this.isModding = true;
-    //TODO Figure out date format
+    
+    if (data.day.includes("Mon")) this.programForm.controls['monday'].patchValue(true);
+    if (data.day.includes("Tues")) this.programForm.controls['tuesday'].patchValue(true);
+    if (data.day.includes("Wed")) this.programForm.controls['wednesday'].patchValue(true);
+    if (data.day.includes("Thur")) this.programForm.controls['thursday'].patchValue(true);
+    if (data.day.includes("Fri")) this.programForm.controls['friday'].patchValue(true);
+    if (data.day.includes("Sat")) this.programForm.controls['saturday'].patchValue(true);
+    if (data.day.includes("Sun")) this.programForm.controls['sunday'].patchValue(true);
+    
     this.programForm.patchValue({
       programName: data.name,
       location: data.location,
       description: data.description,
       fee: data.fee,
       capacity: data.capacity,
-      timeStart: data.startTime,
-      timeEnd: data.endTime,
-      dateStart: data.startDate,
-      dateEnd: data.endDate,
+      timeStart: Utils.createFormTime(data.startTime?.toString()),
+      timeEnd: Utils.createFormTime(data.endTime?.toString()),
+      dateStart: Utils.createFormDate(data.startDate?.toString()),
+      dateEnd: Utils.createFormDate(data.endDate?.toString()),
     });
  }
 
-  createDate(inputDate: string) : string {
-    const year = inputDate.substring(0, inputDate.indexOf("-"));
-    const month = inputDate.substring(inputDate.indexOf("-") +1, inputDate.lastIndexOf("-"));
-    const day = inputDate.substring(inputDate.lastIndexOf("-") + 1);
-
-    return month + "/" + day + "/" + year;
+  /* resetForm
+  /  Resets a form given a form name
+  */
+  resetForm(formName: FormGroup): void {
+    formName.reset();
   }
 
-  createTime(inputTime: string) : string {
-    const hour = inputTime.substring(0,  inputTime.indexOf(":"));
-    const minute = inputTime.substring(inputTime.indexOf(":")); //has ':' in it still
-    const hourValue:number = +hour;
-
-    let retTime = "";
-    
-    if (hourValue < 12&& hourValue != 0){
-      retTime = hourValue  + minute + "am"
-    } else if (hourValue  == 12) {
-      retTime = "12" + minute + "pm";
-    } else if (hourValue  == 0 || hourValue  == 24) {
-      retTime = "12" + minute + "am";
-    }else{
-      retTime = hourValue - 12 +  minute + "pm";
-    }
-    return retTime;
-  }
-
-  openCreateModal(): void {
-    this.isModding = false;
-  }
-
-  removeProgram() : void{
-    // TODO: Need to get id of program we are deleting
-    
-    // Used to test server call. Id changes everytime.
-    let id = "5f8d4d6309b28f3929c16552";
-
+  /* removeProgram
+  /  Removes a program from the database. Calls the api service
+  */
+  removeProgram(id: string) : void{
     this.api.deleteProgram(id).subscribe(result =>{
       if(result['deletedCount'] > 0){
         alert("Delete successful!");
+        this.updateView();
       }
       else{
         alert("Delete failed!");
       }
     })
-  }
-
-  updateProgram() : void {
-
-    let days = [];
-
-    const mon = this.programForm.controls['monday'].value ? "Mon" : null;
-    const tues = this.programForm.controls['tuesday'].value ? "Tues" : null;
-    const wed = this.programForm.controls['wednesday'].value ? "Wed" : null;
-    const thur = this.programForm.controls['thursday'].value ? "Thurs" : null;
-    const fri = this.programForm.controls['friday'].value ? "Fri" : null;
-    const sat = this.programForm.controls['saturday'].value ? "Sat" : null;
-    const sun = this.programForm.controls['sunday'].value ? "Sun" : null;
-
-    if (mon) days.push(mon);
-    if (tues) days.push(tues);
-    if (wed) days.push(wed);
-    if (thur) days.push(thur);
-    if (fri) days.push(fri);
-    if (sat) days.push(sat);
-    if (sun) days.push(sun);
-  
-
-    let body = {
-      name : this.programForm.controls['programName'].value,
-      location : this.programForm.controls['location'].value,
-      description : this.programForm.controls['description'].value,
-      fee : this.programForm.controls['fee'].value,
-      capacity : this.programForm.controls['capacity'].value,
-      startTime : this.createTime(this.programForm.controls['timeStart'].value),
-      endTime: this.createTime(this.programForm.controls['timeEnd'].value),
-      startDate : this.createDate(this.programForm.controls['dateStart'].value),
-      endDate : this.createDate(this.programForm.controls['dateEnd'].value),
-      day : days,
-    };
-
-    if(this.isModding){
-      this.api.modifyProgram(this.programId, body).subscribe();
-    }
-    else{
-      this.api.createProgram(body).subscribe();
-    }
-
-    this.updateView();
   }
 }
--- server/routes/mock.js
+++ server/routes/mock.js
@@ -32,13 +32,32 @@ async function mockUser(){
             staff : false
         },
         {
-            username : "sally.smith",
-            password : bcrypt.hashSync("123456789", 10),
-            firstName : "Sally",
-            lastName : "Smith",
+            username : "anderson.luke",
+            password : bcrypt.hashSync("password", 10),
+            firstName : "Luke",
+            lastName : "Anderson",
             member : false,
             staff : false
+        },
+		{
+            username : "anderson.aini",
+            password : bcrypt.hashSync("password", 10),
+            firstName : "Aini",
+            lastName : "Anderson",
+            member : true,
+            staff : false
+        },
+		{
+            username : "doe.jane",
+            password : bcrypt.hashSync("password", 10),
+            firstName : "Jane",
+            lastName : "Doe",
+            member : true,
+            staff : false
         }
+
+
+
 
     ];
 
@@ -48,27 +67,51 @@ async function mockUser(){
 async function mockProgram(){
     let programs = [
            {
-            name : "Advanced Swimming Lessons",
-            capacity : 10,
-            location : "YMCA pool",
+            name : "Shark",
+            capacity : 7,
+            location : "YMCA Onalaska pool",
             fee : 96,
-            startTime : "1:00pm",
-			endTime : "1:30pm",
-            day : ["Sun", "Mon"],
-			startDate : "10/20/2020",
-		    endDate : "11/20/2020",
-            description : "Participants must pass beginner lessons."
+            startTime : "5:00pm",
+			endTime : "5:40pm",
+            day : ["Sun"],
+			startDate : "12/14/2020",
+		    endDate : "01/10/2021",
+            description : "Participants must have passed pike level before."
+           },
+		   {
+            name : "Shark",
+            capacity : 7,
+            location : "YMCA Onalaska pool",
+            fee : 130,
+            startTime : "6:00pm",
+			endTime : "6:40pm",
+            day : ["Mon", "Wed"],
+			startDate : "12/14/2020",
+		    endDate : "01/10/2021",
+            description : "Participants must have passed pike level before."
            },
            {
             name : "Log Rolling",
-            capacity : 4,
-            location : "YMCA pool",
-            fee : 110,
-            startTime : "2:00pm",
-			endTime : "4:00pm",
-            day : ["Mon", "Wed", "Fri"],
-			startDate : "10/24/2020",
-			endDate :  "11/20/2020",
+            capacity : 0,
+            location : "YMCA Onalaska pool",
+            fee : 200,
+            startTime : "5:00pm",
+			endTime : "5:40pm",
+            day : ["Sun"],
+			startDate : "12/14/2020",
+			endDate :  "01/10/2021",
+            description : "Log rolling is a sport in which two contestants stand on a floating log and try to knock each other off by spinning it with their feet. This is a competitive program with limited capacity."
+           },
+		   {
+            name : "Log Rolling",
+            capacity : 0,
+            location : "YMCA Onalaska pool",
+            fee : 200,
+            startTime : "6:00pm",
+			endTime : "6:40pm",
+            day : ["Mon"],
+			startDate : "12/14/2020",
+			endDate :  "01/10/2021",
             description : "Log rolling is a sport in which two contestants stand on a floating log and try to knock each other off by spinning it with their feet. This is a competitive program with limited capacity."
            },
            {
@@ -79,10 +122,9 @@ async function mockProgram(){
             startTime : "6:00pm",
 			endTime : "7:00pm",
             day : ["Fri"],
-			startDate : "12/1/2020",
+			startDate : "12/01/2020",
 			endDate :  "12/24/2020",
             description : "Learn the benefits of yoga. Participants must be 21 or order to join."
-
            },
            {
             name : "Personal Training Session 1",
@@ -92,8 +134,8 @@ async function mockProgram(){
             startTime : "9:00am",
 			endTime: "10:00am",
             day : ["Tues"],
-			startDate : "1/15/2021",
-			endDate: "2/1/2021",
+			startDate : "01/15/2021",
+			endDate: "02/01/2021",
             description : "1-1 Training sessions with an instructor."
            },
            {
@@ -104,8 +146,8 @@ async function mockProgram(){
             startTime : "9:00am",
 			endTime : "10:00am",
             day : ["Tues, Thurs"],
-			startDate : "1/15/2021",
-			endDate: "2/1/2021",
+			startDate : "01/15/2021",
+			endDate: "02/01/2021",
             description : "1-1 Training sessions with an instructor."
            },
            {
@@ -116,8 +158,8 @@ async function mockProgram(){
             startTime : "3:00pm",
 			endTime:  "4:00pm",
             day : ["Tues"],
-			startDate : "1/15/2021",
-			endDate:  "2/1/2021",
+			startDate : "01/15/2021",
+			endDate:  "02/01/2021",
             description : "1-1 Training sessions with an instructor."
            }
     ];
@@ -125,28 +167,54 @@ async function mockProgram(){
     await Program.insertMany(programs);
 }
 
-/*async function mockUserPrograms(){
-    Program.find({}, (err, programs) =>{
-        User.find({staff : false}, (err, users) => {
-            let j = 0
+async function mockUserPrograms(){
+    Program.find({name: "Shark", startTime: "6:00pm"}, (err, programs) =>{
+        User.find({username: "doe.jane"}, (err, users) => {
             users.forEach(user =>{
-                for(let i = j; i < (3 + j); i++){
-                    user.programs.push(programs[i]._id);
-                }
-                j += 3;
+                user.programs.push(programs[0]._id);
                 user.save();
             })
         });
     })
 
-} */
+	Program.find({name: "Shark", startTime: "6:00pm"}, (err, programs) =>{
+        User.find({username: "anderson.luke"}, (err, users) => {
+            users.forEach(user =>{
+                user.programs.push(programs[0]._id);
+                user.save();
+            })
+        });
+    })
+
+	Program.find({name: "Log Rolling", startTime: "5:00pm"}, (err, programs) =>{
+        User.find({username: "anderson.luke"}, (err, users) => {
+            users.forEach(user =>{
+                user.programs.push(programs[0]._id);
+                user.save();
+            })
+        });
+    })
+
+
+	Program.find({name: "Log Rolling", startTime: "6:00pm"}, (err, programs) =>{
+        User.find({username: "anderson.aini"}, (err, users) => {
+            users.forEach(user =>{
+                user.programs.push(programs[0]._id);
+                user.save();
+            })
+        });
+    })
+
+
+
+}
 
 async function mockData(){
     await mongoose.connection.dropDatabase();
 
     await mockProgram();
     await mockUser();
-   // await mockUserPrograms();
+    await mockUserPrograms();
 }
 
 module.exports = mockData;
